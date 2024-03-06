37c67eaeb6c4ba46b906f85276324bb22adeac2f
rosaysng <160697297+rosaysng@users.noreply.github.com>
2024-03-06 16:39:09 +0700

    Add files via upload

---
+++ Praktikum 3/Standar/Asersi2.java
@@ -0,0 +1,22 @@
+class Lingkaran{
+    private double jariJari;
+    public Lingkaran (double jariJari){
+        this.jariJari = jariJari;
+    }
+    public double hitungkeliling(){
+        double keliling = 2*Math.PI*jariJari;
+        return keliling;
+
+    }
+}
+
+public class Asersi2 {
+    public static void main(String[] args){
+        double jariJari = 0;
+        assert(jariJari>0):"jari jari tidak boleh nol";
+        Lingkaran l = new Lingkaran(jariJari);
+        double kelilingLingkaran = l.hitungkeliling();
+        System.out.println("keliling lingkaran = " + kelilingLingkaran);
+    }
+    
+}
